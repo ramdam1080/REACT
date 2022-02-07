aa99d73a6ba6cb7e0ad324cea98fe1adf1d7c307
mohamed <ramdam.Mohamed1080@hotmail.com>
2022-02-07 08:51:48 +0100

    pierre

--- state/src/App.js
+++ state/src/App.js
@@ -3,15 +3,34 @@ import Perso from "./Perso";
 
 
 export class App extends React.Component{
+  
   constructor(props){
     super(props)
     this.state = ["trum","poutine","macron","kim","Mohamed6"]
+    this.taille = this.state.length
+}
+removeEL=()=>{
+  this.state.shift(); 
+  if(this.taille > 0 ){
+    this.taille = this.state.length
   }
-    taille = this.state.length
+  console.log(this.taille);
+  this.siyessa()
+}
+siyessa=()=>{
+  if(this.taille === 1 ){
+    document.querySelector('body').style.backgroundColor="orange"
+  }else if (this.taille === 0){
+    document.querySelector('body').style.backgroundColor="red"
+    
+  }
+}
   render() {
+    
     return (
       <div>
         <h1>{this.state[4]}</h1>
+        <button onClick={()=>this.removeEL()}>YOUHOUUUUUUUUUUUUUUUUUU</button>
       </div>
     );
   };
